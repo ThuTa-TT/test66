6c31854a220c8589c4c2cece610848c52e0dc329
ThuTa-TT <maungthuta2012@gmail.com>
2023-06-07 23:09:43 +0630

    query builder test

--- app/Http/Controllers/API/AssignController.php
+++ app/Http/Controllers/API/AssignController.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\Department;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class AssignController extends Controller
 {
@@ -22,12 +23,44 @@ class AssignController extends Controller
 
     public function empAssign(Request $request){
         // dd($request);
-        $data1 = Employee::find($request->id);
-        $assign = $data1->assigns;
-        $data1->assign = $assign;
+        // $data1 = Employee::find($request->id);
+        // $assign = $data1->assigns;
+        // $data1->assign = $assign;
         // $data2 = Assign::where('employee_id',$request->id)->whereNull('deleted_at')->get();
         // $emp = $data2->employee;
 
-        return $data1;
+        // $assign_title = DB::table('assigns')->where('employee_id',$request->id)->value('title');
+
+        // $assign_title = DB::table('assigns')->where('employee_id',$request->id)->pluck('title');
+        // foreach ($assign_title as $k => $v) {
+        //    return $k . ','. $v;
+        //     # code...
+        // }
+        // $emp_data = DB::table('employees')->distinct()->get()->toArray();
+
+        // $emps = DB::table('employees')->whereNotBetween('id', [1, 100])->get();
+          
+        // $user = DB::table('employees')->latest() ->first();
+
+        // $randomUser = DB::table('employees')->inRandomOrder()->get();
+
+        // $emp_ass = DB::table('employees')->crossJoin('assigns')->get();
+
+        // $emp_ass = DB::table('employees')->join('assigns', function ($join) {
+        //     $join->on('employees.id', '=', 'assigns.employee_id');
+        // })->get();
+
+        $latestPosts = DB::table('assigns')
+                            ->select('employee_id', DB::raw('MAX(end_date) as last_post_created_at'))
+                            ->where('progress','>',50)
+                            ->groupBy('employee_id');
+        $users = DB::table('employees')
+                    ->joinSub($latestPosts, 'latest_posts', function ($join) {
+                                $join->on('employees.id', '=', 'latest_posts.employee_id');
+                    })->get();
+
+
+       
+        return $users;
     }
 }
