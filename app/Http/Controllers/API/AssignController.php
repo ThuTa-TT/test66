29d36cff18613af0a05c71ec64f76a573caa93bf
ThuTa-TT <maungthuta2012@gmail.com>
2023-06-08 23:30:29 +0630

    orm test

--- app/Http/Controllers/API/AssignController.php
+++ app/Http/Controllers/API/AssignController.php
@@ -50,17 +50,38 @@ class AssignController extends Controller
         //     $join->on('employees.id', '=', 'assigns.employee_id');
         // })->get();
 
-        $latestPosts = DB::table('assigns')
-                            ->select('employee_id', DB::raw('MAX(end_date) as last_post_created_at'))
-                            ->where('progress','>',50)
-                            ->groupBy('employee_id');
-        $users = DB::table('employees')
-                    ->joinSub($latestPosts, 'latest_posts', function ($join) {
-                                $join->on('employees.id', '=', 'latest_posts.employee_id');
-                    })->get();
+        // $latestPosts = DB::table('assigns')
+        //                     ->select('employee_id', DB::raw('MAX(end_date) as last_post_created_at'))
+        //                     ->where('progress','>',50)
+        //                     ->groupBy('employee_id');
+        // $users = DB::table('employees')
+        //             ->joinSub($latestPosts, 'latest_posts', function ($join) {
+        //                         $join->on('employees.id', '=', 'latest_posts.employee_id');
+        //             })->get();
+
+        // $dep = Department::where('dep_name', 'Like', '%Contract Department%')->first();
+        // $department = $dep->fresh();
+        
+        // $dep = Department::where('dep_name', 'Like', '%Contract Department%')->first();
+        // $dep->dep_name='Admin Deparment';
+        // $dep->refresh();
+        // $department = $dep->dep_name;
+
+        // $res = Assign::addSelect(['emp_name' => Employee::select('emp_name')
+        //             ->whereColumn('id', 'assigns.employee_id')
+        //             ->orderByDesc('end_date')
+        //             ->limit(1)
+        //         ])->get();
+
+        $res = Employee::orderByDesc(
+                    Assign::select('end_date')
+                        ->whereColumn('employee_id', 'employees.id')
+                        ->orderByDesc('end_date')
+                        ->limit(1)
+                )->get();       
 
 
        
-        return $users;
+        return $res;
     }
 }
